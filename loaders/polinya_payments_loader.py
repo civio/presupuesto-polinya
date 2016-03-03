0decf55af14f9bbec9ab0db6602ba84d8742e41d
David Cabo <david.cabo@gmail.com>
2016-03-03 06:44:51 +0100

    Create custom payments loader

--- loaders/polinya_payments_loader.py
+++ loaders/polinya_payments_loader.py
@@ -1,6 +1,29 @@
 # -*- coding: UTF-8 -*-
 from budget_app.loaders import PaymentsLoader
+import datetime
 
 class PolinyaPaymentsLoader(PaymentsLoader):
-    # The default class is fine for us
-    pass
+
+    # Parse an input line into fields
+    def parse_item(self, budget, line):
+        # The date is not always available
+        try:
+            date=datetime.datetime.strptime(line[0], "%Y-%m-%d")
+        except ValueError:
+            date=None
+
+        # There are some incomplete lines. Return an empty record.
+        area = line[3].strip()
+        if area == '':
+            return { 'amount': 0 }
+
+        return {
+            'area': area,
+            'fc_code': '',
+            'ec_code': line[5],
+            'date': date,
+            'contract_type': '',
+            'payee': self._titlecase(line[6].strip()),
+            'description': self._spanish_titlecase(line[7].strip()),
+            'amount': self._read_english_number(line[1])
+        }
