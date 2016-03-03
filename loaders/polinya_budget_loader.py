16b13bacbdb7a2319ac539eeb755708f50b2c65a
David Cabo <david.cabo@gmail.com>
2016-03-03 04:50:27 +0100

    Fix Catalan data

--- loaders/polinya_budget_loader.py
+++ loaders/polinya_budget_loader.py
@@ -17,12 +17,31 @@ class PolinyaBudgetLoader(SimpleBudgetLoader):
         # mapping to be constant over time, we are forced to amend budget data prior to 2015.
         # See https://github.com/dcabo/presupuestos-aragon/wiki/La-clasificaci%C3%B3n-funcional-en-las-Entidades-Locales
         programme_mapping = {
+            # old programme: new programme
+            '1340': '1350',
+            '1550': '1530',
+            '1600': '1601',
+            '2400': '2410',
+            '3100': '3110',
+            '3130': '3110',
+            '3210': '3230',
+            '3240': '3260',
+            '3310': '3300',
+            '3320': '3321',
+            '9230': '9232',
+            '9290': '9299',
         }
 
         is_expense = (filename.find('gastos.csv')!=-1)
         is_actual = (filename.find('/ejecucion_')!=-1)
         if is_expense:
-            fc_code = self.clean(line[1])          # Fill with zeroes on the left if needed
+            # We got 3-digit functional codes as input (mostly), so we make them all
+            # into 4-digit ones by adding an extra zero, i.e. left-justify them adding a 0.
+            fc_code = line[1].ljust(4, '0')
+
+            # Institutional codes are 2-digits, we expect them to be 3-digits.
+            # We add a leading zero, this will make code '0' the root, the city hall.
+            ic_code = line[0].rjust(3, '0')
 
             # For years before 2015 we check whether we need to amend the programme code
             year = re.search('municipio/(\d+)/', filename).group(1)
@@ -32,21 +51,21 @@ class PolinyaBudgetLoader(SimpleBudgetLoader):
             return {
                 'is_expense': True,
                 'is_actual': is_actual,
-                'fc_code': fc_code.ljust(4, '0'),
-                'ec_code': self.clean(line[2])[:-2],
-                'ic_code': line[0][:3],
-                'item_number': self.clean(line[2])[-2:],    # Last two digits
+                'fc_code': fc_code,
+                'ec_code': line[2][:-2],        # First three digits (everything but last two)
+                'ic_code': ic_code,
+                'item_number': line[2][-2:],    # Last two digits
                 'description': line[3],
-                'amount': self._parse_amount(line[-1 if is_actual else -4])
+                'amount': self._parse_amount(line[7 if is_actual else 6])
             }
 
         else:
             return {
                 'is_expense': False,
                 'is_actual': is_actual,
-                'ec_code': self.clean(line[0])[:-2],
-                'ic_code': line[0][:3],             # All income goes to the root node
-                'item_number': self.clean(line[0])[-2:],    # Last two digits
+                'ec_code': line[0][:-2],        # First three digits (everything but last two)
+                'ic_code': '000',               # All income goes to the root node
+                'item_number': line[0][-2:],    # Last two digits
                 'description': line[1],
-                'amount': self._parse_amount(line[-1 if is_actual else -4])
+                'amount': self._parse_amount(line[5 if is_actual else 4])
             }
