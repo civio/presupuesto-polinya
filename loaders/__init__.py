ba204864bc9ed84041a2f926dcf8a5289700195e
David Cabo <david.cabo@gmail.com>
2025-03-20 22:22:31 +0100

    Use Python 3-compatible imports civio/presupuesto-management#1286

--- loaders/__init__.py
+++ loaders/__init__.py
@@ -1,2 +1,8 @@
-from polinya_budget_loader import PolinyaBudgetLoader
-from polinya_payments_loader import PolinyaPaymentsLoader
+import six
+
+if six.PY2:
+    from polinya_budget_loader import PolinyaBudgetLoader
+    from polinya_payments_loader import PolinyaPaymentsLoader
+else:
+    from .polinya_budget_loader import PolinyaBudgetLoader
+    from .polinya_payments_loader import PolinyaPaymentsLoader
